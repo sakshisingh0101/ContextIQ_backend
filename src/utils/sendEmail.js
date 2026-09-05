6fb8a89d5d1eeca3cd39afc0f5bfdd94ed2af7f1
sakshisingh0101 <sakshiaingh0101>
2026-09-05 23:30:04 +0530

    sendEmail updated

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,10 +1,45 @@
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+// const sendEmail = async (to, subject, html) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"ContextIQ" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html
+//     });
+
+//     return info; // important
+//   } catch (error) {
+//     console.log("Email Error:", error);
+//     throw error;
+//     // return null;
+//   }
+// };
+
+// export default sendEmail;
+
+
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
@@ -20,7 +55,8 @@ const sendEmail = async (to, subject, html) => {
       html
     });
 
-    return info; // important
+    console.log("Email sent:", info.messageId);
+    return info;
   } catch (error) {
     console.log("Email Error:", error);
     return null;
